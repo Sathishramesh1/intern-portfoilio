96d6f8a5f9ee82a0e8f6d401c5299a88a8009753
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-01-10 21:50:21 +0530

    1s-commit

---
+++ src/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+import Footer from './Footer'
+
+
+function Contact() {
+  return (
+    <>
+    <div className='contact' id='contact'>
+        <h3>Contact</h3>
+        <div className='my-info'>
+
+        
+        <p>Name :Sathish</p>
+        <p>Email:<a href='mailto:sathishrameshkec@gmail.com' target='new'>sathishrameshkec@gmail.com</a></p>
+        <p>Mobile: +91-7010872746</p>
+         </div>
+
+    </div>
+    <Footer/>
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Contact
